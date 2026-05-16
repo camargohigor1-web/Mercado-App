ba47a475dc348c6255fefb81b56346d981dbe675
camargohigor1-web <camargohigor1@gmail.com>
2026-05-16 14:37:25 -0300

    v9.0.0 - feat: migrar para Firebase Firestore com sync em tempo real

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0kU_Tsi-1KnKgj49TP9flhVvcO1YzkI4",
+  authDomain: "mercadoapp-6cd6b.firebaseapp.com",
+  projectId: "mercadoapp-6cd6b",
+  storageBucket: "mercadoapp-6cd6b.firebasestorage.app",
+  messagingSenderId: "701410190492",
+  appId: "1:701410190492:web:dce617b46f34fe28d2f959"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
